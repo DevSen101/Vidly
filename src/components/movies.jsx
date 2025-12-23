6d6fd1797f5e7d518762700a97ccf55d8abf40a8
Dev Sen <devksen.personal@gmail.com>
2025-12-23 16:19:58 +0530

    add: like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+
 class Movies extends Component {
   state = { movies: getMovies() };
 
@@ -7,6 +9,15 @@ class Movies extends Component {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   render() {
     const { length: moviesCount } = this.state.movies.length;
     if (moviesCount === 0) {
@@ -22,6 +33,8 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -31,6 +44,13 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  {" "}
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />{" "}
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
