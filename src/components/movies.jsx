25fe55f7e10768e6bbae4fe09bb67f3c7dfc8aca
Dev Sen <devksen.personal@gmail.com>
2026-01-02 00:26:39 +0530

    feat: sorting and extract table header

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,12 +5,19 @@ import Pagination from "./common/pagination";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
 import MoviesTable from "./common/moviesTable";
+import _ from "lodash";
 
 class Movies extends Component {
-  state = { movies: [], currentPage: 1, pageSize: 4, genres: [] };
+  state = {
+    movies: [],
+    currentPage: 1,
+    pageSize: 4,
+    genres: [],
+    sortColumn: { path: "title", order: "asc" },
+  };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres: genres });
   }
 
@@ -35,12 +42,18 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
+  handleSort = (sortColumn) => {
+    
+    this.setState({ sortColumn });
+  };
+
   render() {
     const { length: count } = this.state.movies;
     const {
       pageSize,
       currentPage,
       selectedGenre,
+      sortColumn,
       movies: allMovies,
     } = this.state;
     if (count === 0) {
@@ -52,7 +65,9 @@ class Movies extends Component {
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
         : allMovies;
 
-    const movies = paginate(filtered, currentPage, pageSize);
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-3">
@@ -76,6 +91,8 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
+            sortColumn={sortColumn}
           />
           <Pagination
             pageSize={pageSize}
